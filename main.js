557fab09fe315c629b7ef48f97ba7a28a01e9e83
laralis <76481905+laralis@users.noreply.github.com>
2023-10-16 17:03:14 -0300

    Ajuste para telas maiores

--- main.js
+++ main.js
@@ -15,11 +15,39 @@ ScrollReveal({
   #contact header,
   #contact .content`);
 window.addEventListener("scroll", onScroll);
-onscroll();
-
+onScroll();
 function onScroll() {
   showNavOnScroll();
   showBackToTopButtonOnScroll();
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+}
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+
+  //verificar se a sessão passou da linha
+  //dados necessários
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+  //verificar se a base esta abaixo da linha alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+
+  //limites da seção
+
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
+  console.log(sectionBoundaries);
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+  menuElement.classList.remove("active");
+
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
 }
 
 function showNavOnScroll() {
@@ -43,4 +71,3 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove("menu-expended");
 }
-console.log("Ola");
